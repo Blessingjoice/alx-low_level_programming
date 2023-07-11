cc681be5a732653de2d9c3cc51902ca5bd527c57
Blessingjoice <blessingnwabuoku@gmail.com>
2023-07-11 12:28:35 -0700

    Arguments and argument counter

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,12 +8,10 @@
  */
 int main(int argc, char const *argv[])
 {
-	int i = 0;
+	int i;
 
-	while (argc--)
-	{
-		printf("%d\n", argv[i]);
-		i++;
-	}
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
 	return (0);
 }
