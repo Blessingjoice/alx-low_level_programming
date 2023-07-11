f7766e3745e41e0b91e1de68fcb754af480dd00a
Blessingjoice <blessingnwabuoku@gmail.com>
2023-07-11 12:19:18 -0700

    Arguments and argument counter

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%d\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
